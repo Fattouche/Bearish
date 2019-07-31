37ae00737ec064a9e314e71a1ff0a4a17ac756bb
fattouche <alex.fattouche@gmail.com>
2019-07-31 11:47:18 +0100

    Reading up on error handling

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,20 @@ fn price(symbol: String) -> String {
     format!("Price for {} is {}", symbol,price)
 }
 
+
+#[get("/buy/<symbol>/<amount>")]
+fn buy(symbol: String, amount: i32) -> String {
+    format!("Bought {} shares of {}", amount, symbol)
+}
+
 // TODO: https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html use reqwest to make client calls to stock api
 fn hit_api(symbol: &String) -> f64{
-     let mut rng = rand::thread_rng();
+    let mut rng = rand::thread_rng();
     rng.gen_range(0.0, 10.0)
 }
 
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![price]).launch();
+    rocket::ignite().mount("/", routes![price,buy]).launch();
 }
