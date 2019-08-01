b91c35ff01a7e837a882c3072d4c8f4a81268889
fattouche <alex.fattouche@gmail.com>
2019-08-01 17:53:54 +0100

    Sell

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,17 @@ fn buy(symbol: String, amount: i32) -> String {
     format!("Bought {} shares of {}", amount, symbol)
 }
 
+#[post("/sell/<symbol>/<amount>")]
+fn sell(symbol: String, amount: i32) -> String {
+    format!("Sold {} shares of {}", amount, symbol)
+}
+
 // TODO: https://rust-lang-nursery.github.io/rust-cookbook/web/clients/apis.html use reqwest to make client calls to stock api
 fn hit_api(symbol: &String) -> f64{
     let mut rng = rand::thread_rng();
     rng.gen_range(0.0, 10.0)
 }
 
-
-
 fn main() {
-    rocket::ignite().mount("/", routes![price,buy]).launch();
+    rocket::ignite().mount("/", routes![price,buy, sell]).launch();
 }
